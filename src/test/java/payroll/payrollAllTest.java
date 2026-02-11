4badc6f291583cf963803d9cd75056af754c0b4e
Darren D <darrenld0730@gmail.com>
2026-02-11 00:03:01 -0500

    d

--- src/test/java/payroll/payrollAllTest.java
+++ src/test/java/payroll/payrollAllTest.java
@@ -8,7 +8,7 @@ class payrollAllTest {
 
 	@Test
 	void test() {
-		payrollAll payroll = new payrollAll();
+		payrollAll p = new payrollAll();
 
         employee c = new Contractor("Alice", 50);
         employee h = new hourly("Horace", 10);
@@ -17,11 +17,11 @@ class payrollAllTest {
         c.setHoursWorked(30);
         h.setHoursWorked(45);
 
-        payroll.addEmployee(c);
-        payroll.addEmployee(h);
-        payroll.addEmployee(s);
+        p.addEmployee(c);
+        p.addEmployee(h);
+        p.addEmployee(s);
 
-        double[] pay = payroll.payAllEmployees();
+        double[] pay = p.payAllEmployees();
 
         assertEquals(1500.0, pay[0], 0.001);
         assertEquals(475.0, pay[1], 0.001);
